e4636de91bf71818f8c714e61fc94c315b882d55
Billy Mathison <wlmathison@gmail.com>
2019-05-28 14:09:55 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace english_idioms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
